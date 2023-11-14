22a178d5813a844e161422e67a5fcbcb80a3111f
Hoàng Diệu Linh <136173788+dieulinh0809@users.noreply.github.com>
2023-11-14 07:50:45 +0700

    Update thctdlgtbai12.cpp

--- thctdlgtbai12.cpp
+++ thctdlgtbai12.cpp
@@ -37,3 +37,141 @@ int main()
     f.close();
 
     //In ra ket qua
+ cout<<"Danh sach LKD P : ";
+    Duyet(P);
+    cout<<endl;
+    //Xu ly danh sach Q
+
+    NODE*p=P.pH;
+    while(p !=NULL){
+        NODE*newNODE = new NODE;
+        newNODE->data=p->data;
+        newNODE->next =Q.pH;
+        Q.pH=newNODE;
+        p=p->next;
+        //AfterInsert(Q,p->data,Q.pH);
+    }
+    //In ra Q
+    cout<<"Danh sach LKD Q : ";
+    Duyet(Q);
+    cout<<endl;
+    //Xoa phan tu du lieu x >> Tim kiem x
+    cout<<"Nhap vao so muon xoa : ";
+    cin>>x;
+    NODE*M=timKiem(P,x);
+    if(M!=NULL){
+        Xoa(P,M);
+        cout<<"Da xoa !";
+        cout<<endl;
+    }else{
+        cout<<"Khong co nut trong danh sach";
+    }
+    cout<<"Danh sach P sau khi xoa : ";
+     Duyet(P);
+   return 0;
+}
+//==Dinh nghia ham ==//
+void khoiTao(LIST& ds){
+    ds.pH=NULL;//Dat dia chi la nut NULL
+    ds.pT=NULL;
+}
+
+int  empty(LIST ds){
+    if(ds.pH==NULL){
+        return 1;
+    }
+    return 0;
+}
+/*NODE*TaoNode(int x){
+    //Tao nut moi
+    NODE*p;
+    p=new NODE ;
+    //neu p==NULL >> ko du bo nho
+    if(p==NULL){
+        cout<<"Khong du bo nho !";
+        return NULL;
+    }
+    p->data=x
+    p->next=NULLL;
+    return p;
+
+}
+*/
+void AfterInsert(LIST&ds ,int x,NODE* M){
+    //Tao nut moi
+    NODE*N= new NODE;
+    N->data=x;
+    N->next=NULL;
+    //Noi nut moi
+    if(ds.pH==NULL){
+        ds.pH=N;
+    }else{
+        N->next = M->next;
+        M->next = N;
+    }
+    return ;
+}
+void BeforeInsert(LIST&ds,int x,NODE*M ){
+    //Tao nut moi
+    NODE*N=new NODE;
+    N->data=x;
+    N->next=NULL;
+    //TH M la nut dau tien
+    if(ds.pH==NULL || ds.pH==M){
+        N->next=M;
+        ds.pH=N;
+    }
+    //TH M la nut o giua
+    //Tim nut truoc nut M
+
+    //Thuc hien bo sung
+    NODE*p=ds.pH;
+    while( p->next != M){
+        //ph->next : truy nhap vao link nut sau ~ Truy nhap nut sau
+        p=p->next;
+    }
+    p->next=N;
+    N->next=M;
+}
+void Xoa(LIST& ds,NODE*M){
+    //TH ds rong
+        if(ds.pH==NULL){
+                cout<<" Danh sach rong !";
+        return ;
+    }
+    //Ngat nut noi voi M
+    //Th M la nut dau tien
+    if(ds.pH==NULL || ds.pH==M){
+            ds.pH=ds.pH->next;
+    }else{
+    //Tim nut trươc M
+    NODE*p=ds.pH;
+    while(p->next !=M){
+            p=p->next;
+    }
+    p->next=M->next;
+    }
+    delete M ;
+    return ;
+}
+void Duyet(LIST& ds){
+    NODE*p=ds.pH;
+    while(p->next!=NULL){
+        cout<<p->data<<" ";
+        p=p->next;
+
+    }
+    return ;
+}
+NODE*timKiem(LIST& ds,int x){
+    NODE*p=ds.pH;
+    while(p->next!=NULL){
+        if(p->data==x){
+            return p;
+        }
+        p=p->next;
+    }
+    return NULL;
+}
+
+
