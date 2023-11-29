8256cf5203bc3ee68645fad12ab87f4fb06ae282
Hoàng Diệu Linh <136173788+dieulinh0809@users.noreply.github.com>
2023-11-29 13:27:17 +0700

    Create dethi1.cpp

---
+++ dethi1.cpp
@@ -0,0 +1,8 @@
+/*
+Bài 1 (5 điểm): Nhập vào từ file "dethi1.txt" chứa 1 dãy số .Nhập vào DSLK đôi , yêu cầu sắp xếp theo giá trị các node và tìm kiếm theo giá trị yêu cầu 
+  Yêu cầu:
+  Triển khai cấu trúc dữ liệu danh sách liên kết đôi.
+  Cài đặt thuật toán sắp xếp danh sách liên kết đôi theo thứ tự giảm dần (sắp xếp theo giá trị các node).
+  Cài đặt thuật toán tìm kiếm trong danh sách liên kết đôi.
+  Thực hiện sắp xếp và tìm kiếm trên một danh sách mẫu gồm ít nhất 15 phần tử.
+*/
